d4bdb681c5fc42146807d23c6a377c48fc575145
Will Torres <52608541+torrwill@users.noreply.github.com>
2024-10-30 14:02:54 -0400

    feat: add MatrixProcessor Class definition

--- helpers/Process_Matrix/process_matrix.py
+++ helpers/Process_Matrix/process_matrix.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sklearn.decomposition import PCA
 from helpers.Timers.timer import run_timer
+from typing import List
 
 # from helpers.Eigne_Calculator.[filename] import [eig_func] as eigen_manual
 from helpers.Principal_Component_Calculator.pca import pca as pca_manual
@@ -14,36 +15,13 @@ def svd_manual(data):
     # Placeholder for the actual manual SVD logic
     return np.array([]), np.array([]), np.array([])
 
-def process_matrix(i, data, labels, execution_times, results_summary):
-    print(f"\nRunning computations on {labels[i]} matrix")
-    
-    # Manual Eigen
-    results, exec_time = run_timer(eigen_manual, data)
-    execution_times['manual_eig'].append(exec_time)
-    results_summary['manual_eig'].append(results)
-
-    # NumPy Eigen
-    results, exec_time = run_timer(np.linalg.eig, data)
-    execution_times['sklearn_eig'].append(exec_time)
-    results_summary['sklearn_eig'].append(results)
-
-    # Manual PCA
-    results, exec_time = run_timer(pca_manual, 2, data)
-    execution_times['manual_pca'].append(exec_time)
-    results_summary['manual_pca'].append(results)
-
-    # NumPy PCA
-    sklearn_pca = PCA(n_components=2)
-    results, exec_time = run_timer(sklearn_pca.fit_transform, data)
-    execution_times['sklearn_pca'].append(exec_time)
-    results_summary['sklearn_pca'].append(results)
-
-    # Manual SVD
-    results, exec_time = run_timer(svd_manual, data)
-    execution_times['manual_svd'].append(exec_time)
-    results_summary['manual_svd'].append(results)
-
-    # Numy SVD
-    results, exec_time = run_timer(np.linalg.svd, data)
-    execution_times['sklearn_svd'].append(exec_time)
-    results_summary['sklearn_svd'].append(results)
+class MatrixProcessor:
+    def __init__(self, labels: List[str], n_components: int = 2):
+        self.labels = labels
+        self.n_components = n_components
+        self.execution_times = {
+            'manual_eig': [], 'sklearn_eig': [],
+            'manual_pca': [], 'sklearn_pca': [],
+            'manual_svd': [], 'sklearn_svd': []
+        }
+        self.results_summary = {key: [] for key in self.execution_times.keys()}
