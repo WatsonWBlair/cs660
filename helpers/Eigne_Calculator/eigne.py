7d61acdcff9e5f623e2328c9f870185863e83974
Watson Blair <watsonwblair@gmail.com>
2024-11-01 15:39:33 -0400

    code cleanup and integration of non-random test data

--- helpers/Eigne_Calculator/eigne.py
+++ helpers/Eigne_Calculator/eigne.py
@@ -1,8 +1,5 @@
 
 import numpy as np
-from sympy.matrices import Matrix, eye
-from sympy import symbols
-import pprint
 
 """eigne function calculates the eigenvalues and eigenvectors for a given covariance matrix"""
 def eigne(covMatrix):
@@ -30,27 +27,3 @@ def identity(shape):
     for i in range(shape[0]):
         result[i][i] = 1
     return result
-
-"""
-else:
-    for i in range(size):
-        mod_array = 
-        multiplier = 1 if i%2 else -1
-        determinate += multiplier*(matrix[0,i] * recursiveDeterminate(mod_array))
-
-return determinate
-"""
-
-"""
- det = 0
-    for i in range(len(matrix)):
-        # Create submatrix by removing the first row and ith column
-        submatrix = [row[:i] + row[i + 1:] for row in matrix[1:]]
-        det += ((-1) ** i) * matrix[0][i] * determinant(submatrix)
-
-    return det
-
-
-"""
-
-
